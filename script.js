7f8c453c49ea9534b83fe3c4bbae74c55997d597
Jason Wu <jwu0592@gmail.com>
2023-08-26 18:44:04 -0400

    finished the transition for the weather page

--- script.js
+++ script.js
@@ -25,14 +25,15 @@ const monthsOfYear = [
 
 const locationButton = document.querySelector('.js-device-location');
 const landingPage = document.querySelector('.js-landing-page');
-
+const weatherPage = document.querySelector('.js-display-weather');
 
 locationButton.addEventListener('click', () =>{
   if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition(showLocation, checkError);
-    landingPage.style.transform = 'translateX(-150px)';
+    transitionPage(weatherPage, landingPage);
   }
 });
+
 
 const checkError = (error) => {
   switch (error.code) {
@@ -69,3 +70,20 @@ ${dayOfWeek}<span> ${month} ${day}, ${year}</span>
 </p>`
 
 document.querySelector(".date-block").innerHTML = dateHTML;
+
+
+const inputField = document.querySelector('.js-city-name-getter');
+
+inputField.addEventListener('keydown', (event) => {
+  if(event.key === 'Enter'){
+    transitionPage(weatherPage, landingPage);
+  }
+});
+
+
+function transitionPage(weatherPage, landingPage){
+  landingPage.style.transform = 'translateX(-193px)';
+  weatherPage.style.transform = 'translateX(200px)';
+  weatherPage.style.opacity = '1';
+  weatherPage.style.backgroundColor = 'rgb(8,44,108)';
+}
